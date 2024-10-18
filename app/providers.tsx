1bdb3f188f9a3e5201cc3c88ac2dea96a08fe728
Franco Carballar <francocarballar@gmail.com>
2024-10-18 18:24:01 -0300

    feat: :sparkles: crea las tarjetas de las miniapps

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,37 +1,25 @@
-"use client"
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-    getDefaultConfig,
-    RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
-import {
-    avalancheFuji
-} from 'wagmi/chains';
-import {
-    QueryClientProvider,
-    QueryClient,
-} from "@tanstack/react-query";
+'use client'
+import '@rainbow-me/rainbowkit/styles.css'
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { WagmiProvider } from 'wagmi'
+import { avalancheFuji } from 'wagmi/chains'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
+export function Providers ({ children }: { children: React.ReactNode }) {
+  const config = getDefaultConfig({
+    appName: 'Sherries',
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID ?? '',
+    chains: [avalancheFuji],
+    ssr: true // If your dApp uses server side rendering (SSR)
+  })
 
+  const queryClient = new QueryClient()
 
-export function Providers({ children }: { children: React.ReactNode }) {
-    const config = getDefaultConfig({
-        appName: 'Sherries',
-        projectId: 'YOUR_PROJECT_ID',
-        chains: [avalancheFuji],
-        ssr: true, // If your dApp uses server side rendering (SSR)
-    });
-    
-    const queryClient = new QueryClient();
-    
-    return (
-        <WagmiProvider config={config}>
-            <QueryClientProvider client={queryClient}>
-                <RainbowKitProvider>
-                    {children}
-                </RainbowKitProvider>
-            </QueryClientProvider>
-        </WagmiProvider>
-    )
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
 }
