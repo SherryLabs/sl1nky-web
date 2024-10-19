bc7f3e30d18cd7bacb11dc3c782a40fa1c24d7bb
José de Jesús Acevedo Flores <jjaf051094@Laptop-de-Jose.local>
2024-10-18 22:32:55 -0300

    update  metadata & add abi.ts

--- sherries/metadata.ts
+++ sherries/metadata.ts
@@ -12,21 +12,17 @@ export interface Metadata<T extends ActionType = 'action'> {
     }
 }
 
-/** If there is parameters we add inputs */
 export interface LinkedAction {
-    /** Type of action to be performed by user */
     type: LinkedActionType;
-    /** URL endpoint for an action */
     href: string;
-    /** button text rendered to the user */
     label: string;
     blockchainActions?: Array<BlockchainAction>;
     extraActions?: Array<LinkedAction>;
 }
 
 export type LinkedActionType =
-    | "blockchain-write" /* User is writing to the blockchain */
-    | "blockchain-read" /* User is reading from the blockchain */
-    | "normal" /* User is performing a normal action */
+    | "blockchain-write" 
+    | "blockchain-read" 
+    | "normal" 
     
 export type ActionType = "action" | "message" | "post" | "external-link";
