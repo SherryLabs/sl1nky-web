2b8fd525c243f1d5782298f7561121be5bccb87f
Franco Carballar <francocarballar@gmail.com>
2024-10-19 15:46:05 -0300

    feat: :triangular_flag_on_post: actualiza la estructura de datos de la metadata

--- sherries/interface/BlockchainAction.ts
+++ sherries/interface/BlockchainAction.ts
@@ -1,17 +1,16 @@
-import { LinkedAction } from '../metadata';
+import { LinkedAction } from '../metadata'
 
 export interface BlockchainAction extends LinkedAction {
-    /** Address of the smart contract */
-    contractAddress: string;
-    /** ABI of the smart contract */
-    contractABI: any;
-    /** Parameters for the blockchain transaction */
-    transactionParameters: Array<any>;
-    /** Type of blockchain action: 'read' or 'write' */
-    blockchainActionType: 'read' | 'write';
-    /** Function name of the smart contract */
-    functionName: string;
-    /** Function signature of the smart contract */
-    functionSignature?: string;
-    
+  /** Address of the smart contract */
+  contractAddress: `0x${string}`
+  /** ABI of the smart contract */
+  contractABI: any
+  /** Parameters for the blockchain transaction */
+  transactionParameters: Array<any>
+  /** Type of blockchain action: 'read' or 'write' */
+  blockchainActionType: 'read' | 'write'
+  /** Function name of the smart contract */
+  functionName: string
+  /** Function signature of the smart contract */
+  functionSignature?: string
 }
