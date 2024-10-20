c3734aae00d72badc726ff4308b36912ba329da1
José de Jesús Acevedo Flores <jjaf051094@Laptop-de-Jose.local>
2024-10-20 08:38:17 -0300

    s

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-  domains: ['ipfs.io', 'tu-dominio.com', "gunzillagames.com"],
+  domains: ['ipfs.io', 'tu-dominio.com', "gunzillagames.com", "miro.medium.com"],
   remotePatterns: [
     {
       protocol: 'https',
